b5b249c46d12593bfd658e77391c91d953894572
yurza <yurzaachmad@gmail.com>
2023-10-01 18:31:20 +0700

    update page chat box automatically scroll to bottom

--- app/components/Chat/ChatList.tsx
+++ app/components/Chat/ChatList.tsx
@@ -3,15 +3,28 @@
 /* Instruments */
 import { useSelector, selectChats } from "@/lib/redux";
 import { ChatItem } from "./ChatItem";
+import styles from "./chat.module.css";
+import { useEffect, useRef } from "react";
 
 export const ChatList = () => {
   const chats = useSelector(selectChats);
+
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chats]);
 
   return (
     <div>
       {chats?.map((item) => (
         <ChatItem key={item._id} message={item} />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
